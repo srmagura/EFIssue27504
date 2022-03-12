c5854d8738d053e7b8f4870fc9c87fcc3da489c4
Sam Magura <srmagura@gmail.com>
2022-03-12 13:26:00 -0500

    Simplify

--- DataContext/DbImport.cs
+++ DataContext/DbImport.cs
@@ -5,11 +5,5 @@ public class DbImport : DbEntity
     public Guid ProjectId { get; protected set; }
     public DbProject? Project { get; protected set; }
 
-    public string Filename { get; protected set; }
     public DbFileRef File { get; protected set; }
-
-    public DateTimeOffset? ProcessingStartUtc { get; protected set; }
-    public DateTimeOffset? ProcessingEndUtc { get; protected set; }
-
-    public string? ErrorMessage { get; protected set; }
 }
--- DataContext/DbProject.cs
+++ DataContext/DbProject.cs
@@ -5,21 +5,7 @@ public class DbProject : DbEntity
     [MaxLength(128)]
     public string Name { get; set; }
 
-    [MaxLength(64)]
-    public string ShortName { get; set; }
-
-    public string Description { get; set; }
-
-    [MaxLength(128)]
-    public string CustomerName { get; set; }
-
-    public int EstimatedSquareFeet { get; set; }
-
     public DbFileRef? Photo { get; set; }
-
-    public bool IsActive { get; set; } = true;
-
-    public DateTimeOffset? DesignerLockedUtc { get; set; }
 
     public List<DbImport> Imports { get; set; } = new();
 }
