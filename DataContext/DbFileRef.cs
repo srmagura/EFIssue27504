77a5b3ceb99109bab8ca94aaf64d59eb2b85d288
Sam Magura <srmagura@gmail.com>
2022-03-12 13:30:23 -0500

    Simplify

--- DataContext/DbFileRef.cs
+++ DataContext/DbFileRef.cs
@@ -3,12 +3,6 @@ namespace DataContext;
 [Owned]
 public record DbFileRef
 {
-    public DbFileRef(Guid fileId, string fileType)
-    {
-        FileId = fileId;
-        FileType = fileType ?? throw new ArgumentNullException(nameof(fileType));
-    }
-
     public Guid FileId { get; protected init; }
 
     [MaxLength(32)]
