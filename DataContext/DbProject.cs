904106cf1aa5624a0fa0bf396bc3899c7002326f
Sam Magura <srmagura@gmail.com>
2022-03-12 13:26:53 -0500

    Simplify

--- DataContext/DbProject.cs
+++ DataContext/DbProject.cs
@@ -5,7 +5,5 @@ public class DbProject : DbEntity
     [MaxLength(128)]
     public string Name { get; set; }
 
-    public DbFileRef? Photo { get; set; }
-
     public List<DbImport> Imports { get; set; } = new();
 }
